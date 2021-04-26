aa9743f55e14eb4304765298a3edab90e82bdd57
taro.yamada <lakjsdfg;lajksd4fmdk;234@gmail.com>
2021-04-26 15:56:09 +0900

    Add Employee new

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -14,21 +14,21 @@ import javax.persistence.Table;
 @Table(name = "employees")
 @NamedQueries({
     @NamedQuery(
-            name="getAllEmployees",
-            query="SELECT e FROM Employee AS e ORDER BY e.id DESC"
-            ),
+        name = "getAllEmployees",
+        query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"
+    ),
     @NamedQuery(
-            name="getEmployeeCount",
-            query="SELECT COUNT(e) FROM Employee AS e"
-            ),
+        name = "getEmployeesCount",
+        query = "SELECT COUNT(e) FROM Employee AS e"
+    ),
     @NamedQuery(
-            name="checkRegisteredCode",
-            query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"
-            ),
+        name = "checkRegisteredCode",
+        query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"
+    ),
     @NamedQuery(
-            name="checkLoginCodeAndPassword",
-            query="SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass"
-            )
+        name = "checkLoginCodeAndPassword",
+        query = "SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass"
+    )
 })
 @Entity
 public class Employee {
@@ -57,6 +57,7 @@ public class Employee {
 
     @Column(name = "delete_flag", nullable = false)
     private Integer delete_flag;
+
 
     public Integer getId() {
         return id;
